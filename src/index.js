e3c4f36c135dd6961525c601dddc340220fddfe5
NancySchmeisser <42864354+NancySchmeisser@users.noreply.github.com>
2019-01-14 13:43:59 +0100

    feat
    Text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Hi() {}
+function Hi() {
+  return <div>Hello <your name>!</div>;
+}
+ReactDOM.render(<hi/>, document.querySelector("#root"));
