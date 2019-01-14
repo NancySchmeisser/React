2c6c1ad3ab6c146083a1f45e5fc3c9767c6b9722
NancySchmeisser <42864354+NancySchmeisser@users.noreply.github.com>
2019-01-14 16:10:27 +0100

    feat
    h1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function Hi() {
-  return <div>Hello <your name>!</div>;
+  return (
+    <div>
+      Hello <strong>your name!</strong>
+      <h1>Heading</h1>
+      {5 + 10}
+    </div>
+  );
 }
-ReactDOM.render(<hi/>, document.querySelector("#root"));
+
+ReactDOM.render(<Hi />, document.querySelector("#root"));
